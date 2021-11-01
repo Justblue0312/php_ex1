fdc5e2cd348b783093787eee00821938391bf892
Justblue0312 <71886798+Justblue0312@users.noreply.github.com>
2021-11-01 12:57:18 +0700

    Add files via upload

---
+++ Controllers/BlogController.php
@@ -0,0 +1,48 @@
+<?php
+	//lay duong dan tu Models
+	include "Models/BlogModel.php";
+	class BlogControllers{
+		use BlogModel;
+		
+		public function __construct(){
+			//lay action từ view
+			$action = isset($_GET["action"])?$_GET["action"]:"admin";
+			switch($action){
+				default:
+				// trỏ read() từ model và gán vào biến
+				$page 	=$this->paginator();
+				$result = $this->read();
+				//in ra thông tin
+				include "View/admin.php";
+				break;
+			case 'delete':
+				$this->delete();
+				header("location:index.php");
+				break;	
+			case 'add':
+				//$result = $this->read();
+				$this->add();	
+				include "View/add.php"; 
+				break;
+			case 'Edit':
+				$row=$this->edit();
+				include "View/Edit.php";
+				$this->editpost();
+				break;
+			case 'userpost':
+				$result = $this->userpost();
+				include"View/userpost.php";
+				break;
+			case 'user':
+				$page 	=$this->paginator();
+				$result = $this->read();
+				include "View/user.php";
+			case 'show':
+				$rows= $this->show();
+				include "View/show.php";
+				break;
+			}
+		}
+	}
+	new BlogControllers();
+?>
